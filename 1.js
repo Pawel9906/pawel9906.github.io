302786ef2dd7b9767e0dd346a4fab2ae3aa6d22e
Pawel9906 <95729499+Pawel9906@users.noreply.github.com>
2021-12-09 23:57:02 +0100

    Add files via upload

--- 1.js
+++ 1.js
@@ -1,19 +1,30 @@
 const col1 = document.getElementById('col1')
 const col2 = document.getElementById('col2')
-const mainButton = document.getElementById('mainButton')
+const repeatButton = document.getElementById('repeatButton')
+const noRepeatButton = document.getElementById('noRepeatButton')
 
-mainButton.addEventListener('click', main)
+repeatButton.addEventListener('click', function () {
+main(false)
+})
+
+
+noRepeatButton.addEventListener('click', function () {
+main(true)
+})
 
 
 function random100 () {
 	return Math.floor( Math.random() * 100 ) + 1
 }
 
-function main() {
+function main(repeat) {
 	const even = []
 	const odd = []
 	for (let i = 0; i< 20; i++) {
 		let num = random100()
+		while (repeat && (even.indexOf(num) !== -1 || odd.indexOf(num) !== -1) ) {
+			num = random100()
+		}
 		if (num % 2 === 0){ 
 			even.push(num)
 		} else {
