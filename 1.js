90d07ca390e343b5c83b0da529d77145b7bab17a
Pawel9906 <95729499+Pawel9906@users.noreply.github.com>
2021-12-09 17:25:32 +0100

    Add files via upload

--- 1.js
+++ 1.js
@@ -1,4 +1,3 @@
-
 const col1 = document.getElementById('col1')
 const col2 = document.getElementById('col2')
 const mainButton = document.getElementById('mainButton')
@@ -7,25 +6,25 @@ mainButton.addEventListener('click', main)
 
 
 function random100 () {
-return Math.floor( Math.random() * 100 ) + 1
+	return Math.floor( Math.random() * 100 ) + 1
 }
 
 function main() {
-const even = []
-const odd = []
-for (let i = 0; i< 20; i++) {
-let num = random100()
-if (num % 2 === 0){ 
-even.push(num)
-} else {
-odd.push(num)
-}
-}
-even.sort( (a,b) => a - b )
-odd.sort( (a,b) => a - b )
-col1.innerHTML = even.join('<br />')
-col2.innerHTML = odd.join('<br />')
+	const even = []
+	const odd = []
+	for (let i = 0; i< 20; i++) {
+		let num = random100()
+		if (num % 2 === 0){ 
+			even.push(num)
+		} else {
+			odd.push(num)
+		}
+	}
+	even.sort( (a,b) => a - b )
+	odd.sort( (a,b) => a - b )
+	col1.innerHTML = even.join('<br />')
+	col2.innerHTML = odd.join('<br />')
 
 
-return even
+	return even
 }
